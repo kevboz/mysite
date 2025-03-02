3786619937e95881cbf333af0dfbf080527280dc
kevboz <64555058+kevboz@users.noreply.github.com>
2025-03-02 12:11:02 -0500

    done profile

--- users/models.py
+++ users/models.py
@@ -6,5 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     location = models.CharField(max_length=100)
+
+    
     def __str__(self):
         return f'{self.user.username} Profile'
